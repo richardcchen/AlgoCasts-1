86d0dd51a3f993f2caec378129d1b4c188fbb093
Richard Chen <richard.c.chen@gmail.com>
2019-02-06 21:58:28 -0500

    finished maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const tracker = {}
+  for (let char of str){
+    if (tracker[char]){
+      tracker[char]++
+    } else {
+      tracker[char] = 1
+    }
+  }
+  let highestNum = 0
+  let highestLetter = ''
+  for (let letter in tracker){
+    if (tracker[letter] > highestNum){
+      highestNum = tracker[letter]
+      highestLetter = letter
+    }
+  }
+  return highestLetter
+}
 
 module.exports = maxChar;
